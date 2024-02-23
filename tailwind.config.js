f18edd363d520b29238e2391a64a65cec396bf2d
emmdeveloper <keshichidera@gmail.com>
2024-02-23 13:50:53 +0100

    added daisyUI theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#2500ff",
+
+          secondary: "#0088ff",
+
+          accent: "#0036ff",
+
+          neutral: "#2b2636",
+
+          "base-100": "#f8ffff",
+
+          info: "#0ba1ff",
+
+          success: "#00b678",
+
+          warning: "#ffc600",
+
+          error: "#ff476b",
+        },
       },
-    },
+    ],
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+};
